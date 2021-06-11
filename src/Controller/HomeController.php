5de131ad3924fd892af919affe19f02a3ec40afa
aureliasegarra <av.segarra@gmail.com>
2021-06-11 14:15:35 +0200

    bootstrap and nav bar ok

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,9 +13,6 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/HomeController.php',
-        ]);
+        return $this->render("home/index.html.twig");
     }
 }
