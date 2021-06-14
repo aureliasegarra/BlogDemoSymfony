c755471f14bdcde8d495035bddb73730f47341db
aureliasegarra <av.segarra@gmail.com>
2021-06-14 15:07:54 +0200

    add validator package

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegisterType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,10 +14,18 @@ class SecurityController extends AbstractController
     /**
      * @Route("/register", name="security_register")
      */
-    public function register(): Response
+    public function register(Request $request): Response
     {
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
+
+        // parsing request
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            // data form treatment
+            
+        } 
 
         return $this->render('security/index.html.twig', [
             'controller_name' => 'Inscription',
