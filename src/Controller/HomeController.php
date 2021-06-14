a4967eb23a8711dd1a6b12b484f73d1ceff2ca00
aureliasegarra <av.segarra@gmail.com>
2021-06-14 12:43:08 +0200

    add categories on right side

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Entity\Category;
 use App\Repository\ArticleRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,13 +22,14 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="home")
      */
-    public function index(): Response 
+    public function index(CategoryRepository $repoCategory): Response 
     {
-
+        $categories = $repoCategory->findAll();
         $articles = $this->repoArticle->findAll();
 
         return $this->render("home/index.html.twig", [
             'articles' => $articles,
+            'categories' => $categories,
         ]);
     }
 
@@ -44,4 +47,19 @@ class HomeController extends AbstractController
         ]);
     }
 
+     /**
+     * @Route("/showArticles/{id}", name="show_article")
+     */
+    public function showArticle(?Category $category): Response
+    {
+        if($category){
+            $articles = $category->getArticles()->getValues();
+        } else {
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render("show/showArticle.html.twig", [
+            'articles' => $articles,
+        ]);
+    }
 }
